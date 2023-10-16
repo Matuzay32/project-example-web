c176843aa849cff3d88426e9a33df7fcbe1c1c1e
Ezequiel Rey <ezequielrey@MacBook-Air-de-Ezequiel.local>
2023-10-16 17:42:45 -0300

    Mejorando el modal al hacer click

--- src/app/Components/Cards.jsx
+++ src/app/Components/Cards.jsx
@@ -1,82 +1,104 @@
-'use client';
-
-// import Image from 'next/image';
+import React from 'react';
 import {
   Box,
   Center,
   Heading,
   Text,
   Stack,
-  Avatar,
-  useColorModeValue,
   Image,
   Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
 } from '@chakra-ui/react';
 
 export default function Cards({ item }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = React.useRef(null);
+
   return (
-    <Center py={6}>
-      <Box
-        maxW={'445px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        p={6}
-        overflow={'hidden'}
-      >
+    <>
+      <Center py={6}>
         <Box
-          h={'210px'}
-          bg={'gray.100'}
-          mt={-6}
-          mx={-6}
-          mb={20}
-          pos={'relative'}
+          maxW={'445px'}
+          w={'full'}
+          bg={'white'}
+          boxShadow={'2xl'}
+          rounded={'md'}
+          p={6}
+          overflow={'hidden'}
         >
-          <Image
-            src={
-              'https://images.unsplash.com/photo-1515248137880-45e105b710e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2988&q=80'
-            }
-            fill
-            alt="Example"
-          />
+          <Box
+            h={'210px'}
+            bg={'gray.100'}
+            mt={-6}
+            mx={-6}
+            mb={20}
+            pos={'relative'}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1515248137880-45e105b710e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2988&q=80"
+              alt="Example"
+            />
+          </Box>
+          <Stack>
+            <Heading
+              mt={10}
+              color={'gray.700'}
+              fontSize={'2xl'}
+              fontFamily={'body'}
+            >
+              {item.title}
+            </Heading>
+            <Text color={'gray.500'}>{item.description}</Text>
+          </Stack>
+          <Stack
+            mt={6}
+            direction={'row'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            spacing={4}
+            mb={4}
+          >
+            <Button
+              onClick={() => onOpen()}
+              w={'full'}
+              mt={8}
+              bg={'teal.400'}
+              color={'white'}
+              rounded={'md'}
+              _hover={{
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg',
+                backgroundColor: 'Teal',
+              }}
+            >
+              {item?.button}
+            </Button>
+          </Stack>
         </Box>
-        <Stack>
-          <Heading
-            mt={10}
-            // eslint-disable-next-line react-hooks/rules-of-hooks
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize={'2xl'}
-            fontFamily={'body'}
-          >
-            {item.title}
-          </Heading>
-          <Text color={'gray.500'}>{item.description}</Text>
-        </Stack>
-        <Stack
-          mt={6}
-          direction={'row'}
-          justifyContent={'center'}
-          alignItems={'center'}
-          spacing={4}
-          mb={4}
-        >
-          <Button
-            w={'full'}
-            mt={8}
-            bg={useColorModeValue('teal.400', 'gray.900')}
-            color={'white'}
-            rounded={'md'}
-            _hover={{
-              transform: 'translateY(-2px)',
-              boxShadow: 'lg',
-              backgroundColor: 'Teal',
-            }}
-          >
-            {item?.button}
-          </Button>
-        </Stack>
-      </Box>
-    </Center>
+      </Center>
+
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{item.modal.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>{item.modal.content}</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="teal" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
--- src/app/constants/cards.js
+++ src/app/constants/cards.js
@@ -1,11 +1,21 @@
 export const data = [
   {
     id: 1,
-    title: 'Enhancing Your Online Presence',
-    description: `Our commitment in the field of marketing goes beyond just creating custom applications. We are here to collaborate with you in the process of optimizing your website and boosting its online visibility.`,
+    title: 'Enhancing Your Online Presence with Code Dramers',
+    description: `At Code Dramers, our commitment in the field of marketing goes beyond just creating custom applications. We are here to collaborate with you in the process of optimizing your website and boosting its online visibility. With a focus on Google Ads and top search engine rankings, we ensure that your website is easily discoverable.`,
     href: 'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80',
     image:
       'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80',
     button: 'See more',
+    modal: {
+      title: 'Enhancing Your Online Presence with Code Dramers',
+      content: `At Code Dramers, we have a dedicated team of experts who specialize in Google Ads management and search engine optimization (SEO). We believe in the power of online advertising and its ability to drive traffic to your website.
+
+      Our Google Ads services are tailored to suit your specific needs. Whether you want to increase website traffic, boost online sales, or promote your brand, we can create and manage effective advertising campaigns that reach your target audience.
+
+      Additionally, we understand the importance of ranking high in Google search results. Our SEO experts employ the latest techniques to optimize your website for search engines, making it easier for potential customers to find your website when they search for relevant keywords.
+
+      With our comprehensive approach to online marketing, we can help you achieve your online goals and enhance your digital presence. Let's work together to make your website more visible and attract the right audience. Contact Code Dramers today to learn more about our services and how we can help you succeed online.`,
+    },
   },
 ];
